94629cc99c48b6be8813f96a326285094b44ca62
Augustine Gyan <augustinehappysunshine369@gmail.com>
2025-01-04 23:52:07 +0000

    role based authorization

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middleware/auth_middleware')
+const adminMiddleware = require('../middleware/admin_middleware')
 
-router.get('/welcome', (req, res) => {
+
+//adding extra layer of auth
+router.get('/welcome', authMiddleware, adminMiddleware,  (req, res) => {
     res.json({
         message: 'Welcome to the Admin Page',
         
